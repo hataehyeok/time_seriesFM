ef3ce10177a2048bdd1e2e0fa5a88945082aeeab
hataehyeok <hth021002@postech.ac.kr>
2024-05-25 23:45:12 +0900

    fix: script run parallel

--- imple.sh
+++ imple.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-# Set CUDA devices
-export CUDA_VISIBLE_DEVICES=0,1,2
+# Function to run a command with specified GPUs
+run_command() {
+    local gpu_ids=$1
+    local command=$2
+    CUDA_VISIBLE_DEVICES=${gpu_ids} nohup ${command} &
+}
 
-# Execute Python scripts with different parameters
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode class_token
+# Commands to run
+commands=(
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode class_token"
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode flatten"
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode pooling --pooling_mode gt"
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode pooling --pooling_mode st"
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode class_token"
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode flatten"
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode pooling --pooling_mode gt"
+    "python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode pooling --pooling_mode st"
+)
 
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode flatten
+# Assign GPU IDs
+gpu_ids=("0" "1" "2")
 
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode pooling --pooling_mode gt
+# Run commands in parallel
+for i in ${!commands[@]}; do
+    gpu_index=$((i % 3))
+    run_command ${gpu_ids[$gpu_index]} "${commands[$i]}"
+done
 
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze false --aggregation_mode pooling --pooling_mode st
-
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode class_token
-
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode flatten
-
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode pooling --pooling_mode gt
-
-python script_ucr_nn_0.py --method_name trf_tc_c_0000 --is_freeze true --aggregation_mode pooling --pooling_mode st
+# Wait for all background jobs to finish
+wait
