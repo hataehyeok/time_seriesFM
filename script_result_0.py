d549517c9d1b8de44792ef082625e694e6fad7f8
hataehyeok <hth021002@postech.ac.kr>
2024-05-26 19:59:35 +0900

    feat: modify result print script

--- script_result_0.py
+++ script_result_0.py
@@ -21,13 +21,11 @@ def check_not_done(method_names, data_config_name, get_data_names):
     is_done = True
     n_not_done = 0
     data_names = get_data_names()
-    for method_name, _ in method_names:
+    for method_name in method_names:
         is_method_done = True
         for data_name in data_names:
-            result_agg_dir = os.path.join(
-                '.', 'result_agg', f'{data_config_name}_{data_name}')
-            result_agg_path = os.path.join(
-                result_agg_dir, f'{method_name}.npz')
+            result_agg_dir = os.path.join('.', 'result_agg', f'{data_config_name}_{data_name}')
+            result_agg_path = os.path.join(result_agg_dir, method_name)
             if not os.path.isfile(result_agg_path):
                 is_method_done = False
                 n_not_done += 1
@@ -39,16 +37,14 @@ def check_not_done(method_names, data_config_name, get_data_names):
     return is_done, n_not_done
 
 
-def print_table(method_names, data_config_name, get_data_names,
-                base_map, print_dataset=True):
-    is_done, n_not_done = check_not_done(
-        method_names, data_config_name, get_data_names)
+def print_table(method_names, data_config_name, get_data_names, print_dataset=True):
+    is_done, n_not_done = check_not_done(method_names, data_config_name, get_data_names)
     if not is_done:
         print(f'total of {n_not_done:d} methods are not done!')
         return
 
     print(',', end='')
-    for _, method_name in method_names:
+    for method_name in method_names:
         print(f'{method_name},', end='')
     print()
 
@@ -60,25 +56,9 @@ def print_table(method_names, data_config_name, get_data_names,
         if print_dataset:
             print(f'{data_name},', end='')
         acc_tests = []
-        for method_name, _ in method_names:
-            result_path = os.path.join(
-                result_dir, f'{method_name}.npz')
-            if 'dist' in method_name:
-                acc_test, _ = get_acc(result_path)
-            else:
-                acc_test_0, acc_valid_0 = get_acc(
-                    result_path)
-                arch = method_name[:3]
-                base_name = base_map[arch]
-                result_path = os.path.join(
-                    result_dir, f'{base_name}.npz')
-                acc_test_1, acc_valid_1 = get_acc(
-                    result_path)
-                if acc_valid_0 > acc_valid_1:
-                    acc_test = acc_test_0
-                else:
-                    acc_test = acc_test_1
-
+        for method_name in method_names:
+            result_path = os.path.join(result_dir, method_name)
+            acc_test, _ = get_acc(result_path)
             acc_tests.append(acc_test)
 
         rank_tests = scipy.stats.rankdata(-np.array(acc_tests))
@@ -97,31 +77,21 @@ def print_table(method_names, data_config_name, get_data_names,
 
 
 def main():
-    arcts = ['lst', 'gru', 'r1d', 'trf', ]
-    arct_names = ['LSTM', 'GRU', 'ResNet', 'XFMR', ]
-    base_map = {'gru': 'gru_c_0000', 'lst': 'lst_c_0000',
-                'r1d': 'r1d_c_0000', 'trf': 'trf_c_0000', }
-
-    pt_tamps = ['_sc_c_0000', '_tv_c_0000',
-                '_mu_c_0000', '_tf_c_0000', '_tc_c_0000', ]
-    pt_names = ['SimCLR', 'TS2Vec', 'MixingUp',  'TF-C',  'TimeCLR', ]
-
     method_names = [
-        ['dist_0000', 'ED', ],
-        ['dist_0001', 'DTW', ],
+        'trf_tc_c_0000_freeze_class_token_gt.npz',
+        'trf_tc_c_0000_freeze_flatten_gt.npz',
+        'trf_tc_c_0000_freeze_pooling_gt.npz',
+        'trf_tc_c_0000_freeze_pooling_st.npz',
+        'trf_tc_c_0000_nofreeze_class_token_gt.npz',
+        'trf_tc_c_0000_nofreeze_flatten_gt.npz',
+        'trf_tc_c_0000_nofreeze_pooling_gt.npz',
+        'trf_tc_c_0000_nofreeze_pooling_st.npz'
     ]
-
-    for arct, arct_name in zip(arcts, arct_names):
-        method_names.append([base_map[arct], arct_name, ])
-        for pt_tamp, pt_name in zip(pt_tamps, pt_names):
-            method_names.append([arct + pt_tamp, pt_name, ])
-
+    
     print_dataset = True
     get_data_names = get_ucr_data_names
-    print_table(method_names, 'ucr_00', get_data_names,
-                base_map, print_dataset=print_dataset)
+    print_table(method_names, 'ucr_00', get_data_names, print_dataset=print_dataset)
 
 
 if __name__ == '__main__':
     main()
-
