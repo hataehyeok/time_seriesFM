326bcfec5f7ceddc5ce67a0b4e7c5a0561d6e7bd
hataehyeok <hth021002@postech.ac.kr>
2024-05-30 08:19:47 +0900

    feat: result print by csv

--- script_result_0.py
+++ script_result_0.py
@@ -7,6 +7,7 @@
 import os
 import numpy as np
 import scipy.stats
+import csv
 from engine.data_io import get_ucr_data_names
 
 
@@ -37,43 +38,36 @@ def check_not_done(method_names, data_config_name, get_data_names):
     return is_done, n_not_done
 
 
-def print_table(method_names, data_config_name, get_data_names, print_dataset=True):
+def print_table_to_csv(method_names, data_config_name, get_data_names, csv_file_path, print_dataset=True):
     is_done, n_not_done = check_not_done(method_names, data_config_name, get_data_names)
     if not is_done:
         print(f'total of {n_not_done:d} methods are not done!')
         return
 
-    print(',', end='')
-    for method_name in method_names:
-        print(f'{method_name},', end='')
-    print()
-
     data_names = get_data_names()
     mean_rank = []
-    for data_name in data_names:
-        result_dir = os.path.join(
-            '.', 'result_agg', f'{data_config_name}_{data_name}')
-        if print_dataset:
-            print(f'{data_name},', end='')
-        acc_tests = []
-        for method_name in method_names:
-            result_path = os.path.join(result_dir, method_name)
-            acc_test, _ = get_acc(result_path)
-            acc_tests.append(acc_test)
 
-        rank_tests = scipy.stats.rankdata(-np.array(acc_tests))
-        mean_rank.append(rank_tests)
-        if print_dataset:
-            for acc_test in acc_tests:
-                print(f'{acc_test:f},', end='')
-            print()
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow([''] + method_names)
 
-    mean_rank = np.array(mean_rank)
-    mean_rank = np.mean(mean_rank, axis=0)
-    print(f'Average Rank,', end='')
-    for val in mean_rank:
-        print(f'{val:f},', end='')
-    print()
+        for data_name in data_names:
+            result_dir = os.path.join(
+                '.', 'result_agg', f'{data_config_name}_{data_name}')
+            acc_tests = []
+            for method_name in method_names:
+                result_path = os.path.join(result_dir, method_name)
+                acc_test, _ = get_acc(result_path)
+                acc_tests.append(acc_test)
+
+            rank_tests = scipy.stats.rankdata(-np.array(acc_tests))
+            mean_rank.append(rank_tests)
+            if print_dataset:
+                csvwriter.writerow([data_name] + acc_tests)
+
+        mean_rank = np.array(mean_rank)
+        mean_rank = np.mean(mean_rank, axis=0)
+        csvwriter.writerow(['Average Rank'] + list(mean_rank))
 
 
 def main():
@@ -90,7 +84,7 @@ def main():
     
     print_dataset = True
     get_data_names = get_ucr_data_names
-    print_table(method_names, 'ucr_00', get_data_names, print_dataset=print_dataset)
+    print_table_to_csv(method_names, 'ucr_00', get_data_names, 'test2.csv', print_dataset=print_dataset)
 
 
 if __name__ == '__main__':
